d549001ef729595cf3d5cafe1006016d48c8ec02
Patryk Klatka <patryk.klatka@gmail.com>
2024-06-26 14:43:56 +0200

    Use specific module versions in `ares_setup` script (#19)

--- scripts/ares_setup.sh
+++ scripts/ares_setup.sh
@@ -1,4 +1,4 @@
-module load pro-viz
-module load wayland
-module load cuda
-module load cmake
+module load pro-viz/1.9.2
+module load wayland/1.21.0
+module load cuda/12.2.0
+module load cmake/3.26.3
