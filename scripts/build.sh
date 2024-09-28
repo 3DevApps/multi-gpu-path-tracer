edf1ab2dfdf8cfaa56e6fbac9a550c72a25fab86
Patryk Klatka <patryk.klatka@gmail.com>
2024-09-29 00:10:23 +0200

    feat: add utils

--- scripts/build.sh
+++ scripts/build.sh
@@ -3,13 +3,12 @@
 #SBATCH -p plgrid-gpu-v100
 #SBATCH -A plgggpuinz2024proper-gpu
 #SBATCH --gpus=1
-#SBATCH -t 00:20:00
-#SBATCH --output="output.out"
-#SBATCH --error="error.err"
+#SBATCH -t 00:15:00
+#SBATCH --output=tmp/compile-output.txt
+#SBATCH --error=tmp/compile-output.txt
 
 cd ~/multi-gpu-path-tracer
-rm output.out error.err
 
-source ./scripts/ares_setup.sh 
+source ./scripts/ares_setup.sh
 cmake -S . -B build
-cmake --build build
+cmake --build build "$@"
