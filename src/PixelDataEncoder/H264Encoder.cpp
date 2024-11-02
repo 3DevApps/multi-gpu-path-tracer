6693d5ed6a40cf3b7d019b2534c69f6d1a4dec40
Patryk Klatka <patryk.klatka@gmail.com>
2024-11-02 19:01:45 +0100

    chore: remove unused code

--- src/PixelDataEncoder/H264Encoder.cpp
+++ src/PixelDataEncoder/H264Encoder.cpp
@@ -48,13 +48,12 @@ H264Encoder::~H264Encoder()
 
 bool H264Encoder::encodePixelData(const std::vector<uint8_t> &pixelData, const int width, const int height, std::vector<uint8_t> &outputData)
 {
-    // Convert RGB to I420
     convertRGBtoI420(pixelData);
 
     x264_nal_t *nals;
     int num_nals;
 
-    // Force keyframe for first frame
+    // Force keyframe for the first frame
     if (first_frame)
     {
         pic_in.i_type = X264_TYPE_IDR;
@@ -67,7 +66,6 @@ bool H264Encoder::encodePixelData(const std::vector<uint8_t> &pixelData, const i
     }
 
     outputData.clear();
-    // Concatenate NAL units with start codes
     for (int i = 0; i < num_nals; i++)
     {
         // Add start code (0x00000001)
@@ -83,4 +81,34 @@ bool H264Encoder::encodePixelData(const std::vector<uint8_t> &pixelData, const i
     }
 
     return true;
+}
+
+void H264Encoder::convertRGBtoI420(const std::vector<uint8_t> &rgbData)
+{
+    uint8_t *y = pic_in.img.plane[0];
+    uint8_t *u = pic_in.img.plane[1];
+    uint8_t *v = pic_in.img.plane[2];
+
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            int rgb_index = (i * width + j) * 3;
+            int y_index = i * width + j;
+            int uv_index = (i / 2) * (width / 2) + (j / 2);
+
+            uint8_t r = rgbData[rgb_index];
+            uint8_t g = rgbData[rgb_index + 1];
+            uint8_t b = rgbData[rgb_index + 2];
+
+            // RGB to YUV conversion
+            y[y_index] = ((66 * r + 129 * g + 25 * b + 128) >> 8) + 16;
+
+            if (i % 2 == 0 && j % 2 == 0)
+            {
+                u[uv_index] = ((-38 * r - 74 * g + 112 * b + 128) >> 8) + 128;
+                v[uv_index] = ((112 * r - 94 * g - 18 * b + 128) >> 8) + 128;
+            }
+        }
+    }
 }
